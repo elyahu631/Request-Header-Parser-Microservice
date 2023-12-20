0f5bbb42e5728585692db1bf5dbd335ec6468bc6
Elyahu Anavi <84002556+elyahu631@users.noreply.github.com>
2023-12-20 08:04:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 
 // init project
 require("dotenv").config();
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors");
+const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -43,6 +43,6 @@ app.get("/api/whoami", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
